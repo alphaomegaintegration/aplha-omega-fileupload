a93e885889c574a42f2461c7db9d48e7c5c741b6
Junaid Qureshi <junaid.qureshi@alphaomega.com>
2024-06-28 16:19:05 -0400

    first checkin for the project

---
+++ src/main/java/com/alpha/omega/fileupload/AplhaOmegaFileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.omega.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplhaOmegaFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplhaOmegaFileuploadApplication.class, args);
+	}
+
+}
